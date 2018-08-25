cea9d1443bc0c7ab5bb9295bdf3499337748f7ee
hitheima <hitheima@163.com>
2018-08-25 10:40:51 +0800

    完善登录脚本

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,5 +1,8 @@
+import pytest
+
 from base.base_driver import init_driver
 from page.page import Page
+from base.base_analyze import analyze_with_file
 
 
 class TestLogin:
@@ -8,10 +11,15 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    def test_login(self):
+    @pytest.mark.parametrize("args", analyze_with_file("login_data", "test_login"))
+    def test_login(self, args):
+        username = args["username"]
+        password = args["password"]
+        expect = args["expect"]
+
         self.page.home.click_mine()
         self.page.mine.click_login_sign_up()
-        self.page.login.input_username("13800138006")
-        self.page.login.input_password("123456")
+        self.page.login.input_username(username)
+        self.page.login.input_password(password)
         self.page.login.click_login()
-        assert self.page.login.is_toast_exist("登录成功")
+        assert self.page.login.is_toast_exist(expect)
